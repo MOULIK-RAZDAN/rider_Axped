40fa3093c6c42035257bd2292a9c0e6974061980
New <razdanmoulik@gmail.com>
2020-10-14 15:47:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rider_axped/common/common.java
@@ -0,0 +1,8 @@
+package com.example.rider_axped.common;
+
+import com.example.rider_axped.Model.RiderModel;
+
+public class common {
+    public static final String RIDER_INFO_REFENCE = "Riders";
+    public static RiderModel currentRider;
+}
